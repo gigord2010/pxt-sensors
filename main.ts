5feaf4519cac5c0b4a2d222a73b896240bb26e4a
gigord2010 <38643218+gigord2010@users.noreply.github.com>
2018-05-17 17:37:33 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 //% weight=0 color=#3CB371 icon="\uf2db" block="sensors"
 namespace sensors {
 	//% blockId=DDMmotor block="Mcontrol pin %McontrolPin|direction of Mcontrol(0~1) %McontrolValue|MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
-    //% McontrolValue.min=0 McontrolValue.max=1 MSpeedValue.min=0 MSpeedValue.max=255
+    //% McontrolValue.min=0 McontrolValue.max=1 
+	//% MSpeedValue.min=0 MSpeedValue.max=255
     //% McontrolPin.fieldEditor="gridpicker" McontrolPin.fieldOptions.columns=4
     //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
 	//% MSpeedPin.fieldEditor="gridpicker" MSpeedPin.fieldOptions.columns=4
     //% MSpeedPin.fieldOptions.tooltips="false" MSpeedPin.fieldOptions.width="300"
     
-    export function DDMmotor(McontrolPin: AnalogPin,McontrolValue: number,MSpeedPin: AnalogPin,MSpeedValue: number): void {
+    export function DDMmotor(McontrolPin: AnalogPin,McontrolValue: number1,MSpeedPin: AnalogPin,MSpeedValue: number2): void {
         pins.analogWritePin(McontrolPin, pins.map(McontrolValue, 0, 1, 0, 1023));
         pins.analogWritePin(MSpeedPin, pins.map(MSpeedValue, 0, 255, 0, 1023));
 	   
