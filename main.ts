ecb9c335f994970e5b85f2c832c70042d634e933
gigord2010 <38643218+gigord2010@users.noreply.github.com>
2018-05-17 17:40:03 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% weight=0 color=#3CB371 icon="\uf2db" block="sensors"
 namespace sensors {
 	//% blockId=DDMmotor block="Mcontrol pin %McontrolPin|direction of Mcontrol(0~1) %McontrolValue|MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
-    //% McontrolValue.min=0 McontrolValue.max=1 
+    //% McontrolValue.min=0 McontrolValue.max=255 
 	//% MSpeedValue.min=0 MSpeedValue.max=255
     //% McontrolPin.fieldEditor="gridpicker" McontrolPin.fieldOptions.columns=4
     //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
