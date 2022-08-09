a8be3a755de8927c94f756fc46b2bccac330cee8
gigord2010 <38643218+gigord2010@users.noreply.github.com>
2022-08-09 11:56:52 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=0 color=#3CB371 icon="\uf2db" block="sensors"
 namespace sensors {
-	//% blockId=DDMmotor H="channel H|direction of Mcontrol(0~1) %McontrolValue|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
+	//% blockId=DDMmotor block="Mcontrol p1|direction of Mcontrol(0~1) %McontrolValue|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
     //% McontrolValue.min=0 McontrolValue.max=1 
 	//% MSpeedValue.min=0 MSpeedValue.max=255
     //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
